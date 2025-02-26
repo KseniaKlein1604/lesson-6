f925c7cc53bc77fada60cfea5974300b5c39d73b
KseniaKlein1604 <kseniarayevskaya1999@gmail.com>
2025-02-26 17:44:11 +0300

    lesson6_3.py

--- lesson6_3.py
+++ lesson6_3.py
@@ -11,7 +11,7 @@ driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
 WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR,"text"))
+    EC.presence_of_element_located((By.CSS_SELECTOR,"text"),"Done")
 )
 
 third_image_src=driver.find_element(By.CSS_SELECTOR, "#award").get_attribute("third_image_src")
